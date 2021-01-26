91b52dc4db8bddac3883a76c2f7c147c107dba85
khamaniram <banti.sharma@gmail.com>
2021-01-26 10:29:42 +0530

    Testing zabbix rebranding

---
+++ ui/local/conf/brand.conf.php
@@ -0,0 +1,10 @@
+<?php
+
+//PHP5.6+
+return [
+  'BRAND_HELP_URL' => 'https://www.linkedin.com',
+  'BRAND_LOGO' => 'Graphoun',
+  'BRAND_LOGO_SIDEBAR'=>'./assets/img/graphoun.jpg',
+  'BRAND_LOGO_SIDEBAR_COMPACT'=>'./assets/img/graphoun.jpg',
+  'BRAND_FOOTER' => './assets/img/graphoun.jpg'
+];
