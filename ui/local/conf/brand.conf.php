462d641e572e71b7068dedbd36181003a14d6c3c
khamaniram <banti.sharma@gmail.com>
2021-01-29 14:56:29 +0530

    Made logo changes

--- ui/local/conf/brand.conf.php
+++ ui/local/conf/brand.conf.php
@@ -3,8 +3,8 @@
 //PHP5.6+
 return [
   'BRAND_HELP_URL' => 'https://www.linkedin.com',
-  'BRAND_LOGO' => 'Graphoun',
-  'BRAND_LOGO_SIDEBAR'=>'./assets/img/graphoun.jpg',
-  'BRAND_LOGO_SIDEBAR_COMPACT'=>'./assets/img/graphoun.jpg',
-  'BRAND_FOOTER' => './assets/img/graphoun.jpg'
+  'BRAND_LOGO' => './assets/img/pinterest_profile_image.png',
+  'BRAND_LOGO_SIDEBAR'=>'./assets/img/pinterest_profile_image.png',
+  'BRAND_LOGO_SIDEBAR_COMPACT'=>'./assets/img/pinterest_profile_image.png',
+  'BRAND_FOOTER' => './assets/img/pinterest_profile_image.png'
 ];
